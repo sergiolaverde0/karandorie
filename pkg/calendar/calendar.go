f9759442b00dd7619993d273ea8888480cb6345a
Sergio Laverde <sergiolaverde0@gmail.com>
2025-07-20 18:58:10 -0500

    refactor: store the selected day as a Time instead of an int
    Storing the selected day as a Time object allows for easier update of
    the month to be displayed, and makes the cutom Date struct redundant.
    
    The option to select days and to highlight the current date have been
    removed temporarily.

--- pkg/calendar/calendar.go
+++ pkg/calendar/calendar.go
@@ -8,13 +8,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-// Date represents a date
-type Date struct {
-	Year  int
-	Month int
-	Day   int
-}
-
 // Define keybindings for the calendar
 type keyMap struct {
 	nextMonth key.Binding
@@ -41,12 +34,11 @@ func (k keyMap) ShortHelp() []key.Binding {
 type CalendarModel struct {
 	currentMonth time.Month
 	currentYear  int
-	selectedDate *Date
 	currentView  string
 	monthName    string
 	daysInMonth  int
 	days         [][]bool
-	currentDay   int
+	selectedDay  time.Time
 	keyMap       keyMap
 }
 
@@ -93,42 +85,17 @@ func InitialModel() CalendarModel {
 	model := CalendarModel{
 		currentMonth: t.Month(),
 		currentYear:  t.Year(),
-		selectedDate: &Date{
-			Year:  t.Year(),
-			Month: int(t.Month()),
-			Day:   t.Day(),
-		},
-		currentView: "month",
-		monthName:   t.Month().String(),
-		currentDay:  t.Day(),
-		days:        make([][]bool, 6),
-		keyMap:      keyMap,
-	}
-	// Get the first day of the month
-	firstDay := time.Date(model.currentYear, model.currentMonth, 1, 0, 0, 0, 0, time.UTC)
-	lastDay := time.Date(model.currentYear, model.currentMonth+1, 1, 0, 0, 0, 0, time.UTC).AddDate(0, 0, -1)
-
-	model.daysInMonth = lastDay.Day()
-	for i := range model.days {
-		model.days[i] = make([]bool, 7)
-	}
-	// Find the first day of the month (0=Sunday, 6=Saturday)
-	dayOfWeek := int(firstDay.Weekday())
-	for day := range dayOfWeek {
-		model.days[0][day] = false // blank days before first day
-	}
-	for i := 0; i < model.daysInMonth-1; i++ {
-		// Calculate the row and column
-		row := (i + dayOfWeek) / 7
-		col := (i + dayOfWeek) % 7
-		model.days[row][col] = true
+		monthName:    t.Month().String(),
+		selectedDay:  t,
+		days:         make([][]bool, 7),
+		keyMap:       keyMap,
 	}
 	return model
 }
 
 // Update handles messages
 func (m CalendarModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	lastDay := time.Date(m.currentYear, m.currentMonth+1, 1, 0, 0, 0, 0, time.UTC).AddDate(0, 0, -1)
+	initialDate := m.selectedDay
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
@@ -137,20 +104,20 @@ func (m CalendarModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case key.Matches(msg, m.keyMap.prevMonth):
 			m.PreviousMonth()
 		case key.Matches(msg, m.keyMap.nextDay):
-			m.currentDay += 1
+			m.selectedDay = m.selectedDay.AddDate(0, 0, 1)
 		case key.Matches(msg, m.keyMap.prevDay):
-			m.currentDay -= 1
+			m.selectedDay = m.selectedDay.AddDate(0, 0, -1)
 		case key.Matches(msg, m.keyMap.nextWeek):
-			m.currentDay += 7
+			m.selectedDay = m.selectedDay.AddDate(0, 0, 7)
 		case key.Matches(msg, m.keyMap.prevWeek):
-			m.currentDay -= 7
-		case key.Matches(msg, m.keyMap.selectDay):
-			m.SelectDate()
+			m.selectedDay = m.selectedDay.AddDate(0, 0, -7)
 		case key.Matches(msg, m.keyMap.quit):
 			return m, tea.Quit
 		}
-		return m, nil
 	}
+	m.currentMonth = m.selectedDay.Month()
+	m.currentYear = m.selectedDay.Year()
+	m.updateDays()
 	return m, nil
 }
 
@@ -177,11 +144,6 @@ func (m *CalendarModel) PreviousMonth() {
 	m.updateDays()
 }
 
-// SelectDate selects the current day
-func (m *CalendarModel) SelectDate() {
-	m.selectedDate.Day = m.currentDay
-}
-
 // updateDays builds the calendar grid
 func (model *CalendarModel) updateDays() {
 	// Get the first day of the month
@@ -197,7 +159,7 @@ func (model *CalendarModel) updateDays() {
 	for day := range dayOfWeek {
 		model.days[0][day] = false // blank days before first day
 	}
-	for i := 0; i < model.daysInMonth; i++ {
+	for i := range model.daysInMonth {
 		// Calculate the row and column
 		row := (i + dayOfWeek) / 7
 		col := (i + dayOfWeek) % 7
@@ -221,7 +183,7 @@ func (m CalendarModel) View() string {
 		for j := range m.days[i] {
 			if m.days[i][j] {
 				// Highlight selected day
-				if dayPrinted == m.currentDay {
+				if dayPrinted == m.selectedDay.Day() {
 					sb = append(sb, fmt.Sprintf("\x1b[1;32m%3d\x1b[0m ", dayPrinted)...)
 				} else {
 					sb = append(sb, fmt.Sprintf("%3d ", dayPrinted)...)
